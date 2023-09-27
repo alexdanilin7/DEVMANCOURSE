4ed7161756f14e29c47f74c2409db892aff1bf8c
andanilin <andanilin7@gmail.com>
2023-09-28 00:41:03 +0200

    lesson 7 correct

--- main_api.py
+++ main_api.py
@@ -2,22 +2,27 @@ import requests
 
 
 def get_weather(place):
-    url = f'https://wttr.in/{place}?nTq&lang=ru&M'
-    url_new = f'http://wttr.dvmn.org/{place}?nTqu&lang=ru'
+    try:
+        url = f'https://wttr.in/{place}?nTq&lang=ru&M'
+        url_new = f'http://wttr.dvmn.org/{place}?nTq&lang=ru&M'
 
-    response = requests.get(url)
-    if response.status_code == 200:
-        print(response.text)
-    else:
-        response = requests.get(url_new)
-        print(response.text)
+        response = requests.get(url)
+        response.raise_for_status()
+        if response.status_code == 200:
+            print(response.text)
+        else:
+            response = requests.get(url_new)
+            response.raise_for_status()
+            print(response.text)
+    except Exception as e:
+        print(e)
 
-def get_for_place():
+
+def get_weather_for_place():
     places = ['london', 'svo', 'cherepovec']
     for place in places:
-         get_weather(place)
+        get_weather(place)
 
 
 if __name__ == '__main__':
-    get_for_place()
-
+    get_weather_for_place()
